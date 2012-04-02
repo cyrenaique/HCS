c6f6c497b5ad6fdaf1658c9ee0dddeffad86b49d
Arnaud Ogier <arnaud.ogier@ip-korea.org>
2012-04-02 12:48:52 +0900

    seven commit

---
+++ Controls/PlateChart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace HCSAnalyzer
+{
+    public class PlateChart : Chart
+    {
+        public PlateChart()
+        {
+            SetStyle(System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer, true);
+        }
+    }
+}
